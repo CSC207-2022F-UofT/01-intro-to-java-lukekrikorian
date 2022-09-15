e5740cbf385bbc2d0a58887a64e38e5d79fd8a45
Luke Krikorian <lukekrikorian@gmail.com>
2022-09-15 16:02:11 -0400

    Complete necessary methods

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -43,6 +43,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
+        System.out.println("Hello World!");
 
 
 
@@ -62,8 +63,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
-
-
+        int my_variable = 100;
 
         /* Do not remove the line below: if you did task 2 correctly, then
          * you should see the message "my_variable's value is 100" if you run
@@ -99,8 +99,9 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
-
-
+        for (int i = 10; i >= 0; i--){
+            System.out.println("Current count: " + i);
+        }
     }
 
     /**
@@ -142,7 +143,12 @@ public class Basics {
          */
         StringBuilder ret = new StringBuilder();
 
-        // Fill in the rest of the body here
+        String[] parts = to_split.split(" ");
+
+        for (int i = 0; i < parts.length; i++) {
+            char letter = parts[i].charAt(0);
+            ret.append(letter);
+        }
 
         return ret.toString();
     }
@@ -170,6 +176,12 @@ public class Basics {
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
+
+        if (arr.length < 2) { return 0; }
+
+        for (int i = 1; i < arr.length; i += 2) {
+            current_sum += arr[i];
+        }
 
         return current_sum;
     }
